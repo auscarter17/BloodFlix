15e52cb3258da6f2af5841db5bf7f71e3a33ac1d
acarter <98193003+auscarter17@users.noreply.github.com>
2023-02-20 19:21:56 -0500

    updated fetch, created fallback statement for testing fetch

--- bloodflix/src/MovieDiscover.js
+++ bloodflix/src/MovieDiscover.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MovieDiscover.css'
 
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=293c14281a8d636285d1278d6f4da4f7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27&';
@@ -7,49 +7,38 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
 export default function MovieDiscover() {
     
-    getMovies(API_URL)
+    const [movies, setMovies] = useState([]);    
+
     
-    async function getMovies(url) {
-        const res = await fetch(url)
-        const data = await res.json()
+    useEffect(() => {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setMovies(data);
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }, [])
+    
 
-        getMovieData(data.results)
-    };
+    
+    if (!movies.length) return(
+        <div>No Data</div>
+    )
 
-    function getMovieData(movies) {
 
-        function getClassByRating(vote) {
-            if (vote >= 8) {
-                return 'green'
-            } else if (vote >= 5) {
-                return 'orange'
-            } else {
-                return 'red'
+    return (
+        <div id="main">
+            {movies.map((movie) => {
+                <div key={movie.id} className='movie'>
+                    <h3>{movie.name}</h3>
+                </div>
+            })}
+        </div>
+    )
+
+    
             }
-        }
-        
-        // movies.forEach((movie) => {
-             const { title, poster_path, vote_average, overview } = movies;
 
-            return (
-                <main id="main">
-                    {movies.map((movie) => (
-                        <div key="movie" class="movie">
-                        <img src={IMG_PATH + poster_path} alt={title}/>
-                        <div class="movie-info">
-                            <h3>{title}</h3>
-                            <span class={getClassByRating(vote_average)}>{vote_average}</span>
-                        </div>
-                        <div class="overview">
-                            <h3>Overview</h3>
-                            {overview}
-                        </div>
-                    </div>
-                    ))}
-                    
-                </main>
-            )
-    };
-
-    
-}
