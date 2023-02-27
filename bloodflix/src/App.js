90db4acdc34951c3a8b8d294a611d3728566413c
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2023-02-27 16:54:39 -0500

    added search bar with API search link

--- bloodflix/src/App.js
+++ bloodflix/src/App.js
@@ -1,8 +1,10 @@
 import MovieDiscover from "./MovieDiscover";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <MovieDiscover />
     </div>
   );
---
+++ bloodflix/src/Search.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import './Search.css';
+
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=293c14281a8d636285d1278d6f4da4f7&query="';
+
+
+export default function Search() {
+  const [searchFilter, setSearchFilter] = useState([]);
+
+
+
+  return (
+    <div className='search'>
+      <input type='text' className='input' placeholder='Search...'/>
+        <button className='btn'>
+          <i className='fas fa-search'></i>
+        </button>
+    </div>
+  )
+}
